041d7794aee305f78c1c3ba2b3a4c39c5435ccef
Jason Spiller <jason@thetargetlab.com>
2018-05-02 14:27:01 -0600

    External routes are working.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Header from './components/Header.js';
-import Home from './components/Home.js';
-import TodosContainer from './containers/TodosContainer.js';
-
+import Header from './components/Header';
+import Routes from './config/routes';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
 				<Header />
-				<Switch>
-					<Route exact path='/' component={ Home }/>
-					<Route path='/todos' component={ TodosContainer }/>
-				</Switch>
+				{Routes}
       </div>
     );
   }
