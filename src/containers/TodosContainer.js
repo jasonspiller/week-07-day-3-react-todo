8f84a72d4581eba6d8dddb0f22f89ac6c976849d
Jason Spiller <jason@thetargetlab.com>
2018-05-02 14:37:14 -0600

    JSON is working.

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import TodoModel from '../models/Todo'
 
 
 class TodosContainer extends Component {
   render() {
+		TodoModel.all().then( (res) => {
+			console.log(res);
+		})
     return (
       <div>
         <h2>Todos</h2>
