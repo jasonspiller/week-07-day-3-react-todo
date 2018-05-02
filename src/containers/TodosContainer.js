52c129d095e19ed42b496f199dc5d8190ee19826
Jason Spiller <jason@thetargetlab.com>
2018-05-02 14:54:19 -0600

    Read is working.

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-import TodoModel from '../models/Todo'
+import TodoModel from '../models/Todo';
+import Todos from '../components/Todos';
 
 
 class TodosContainer extends Component {
-  render() {
-		TodoModel.all().then( (res) => {
-			console.log(res);
-		})
+  constructor(){
+    super()
+    this.state = {
+      todos: []
+    }
+  }
+  componentDidMount(){
+    this.fetchData()
+  }
+  fetchData(){
+    TodoModel.all().then( (res) => {
+      this.setState ({
+        todos: res.data.todos,
+        todo: ''
+      })
+    })
+  }
+  render(){
     return (
-      <div>
-        <h2>Todos</h2>
+      <div className="todosComponent">
+        <Todos
+          todos={this.state.todos} />
       </div>
-    );
+    )
   }
 }
 
